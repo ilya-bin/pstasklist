507e2e1dfd089d942ba0e42fc17df6ce8d9c62d5
Ilya <ilya.babentsov@gmail.com>
2023-12-12 16:39:07 +0100

    Update pstasklist.ps1

--- pstasklist.ps1
+++ pstasklist.ps1
@@ -1,5 +1,5 @@
 ##################################################
-#  Task list in with CPU % and memory in PowerShell 
+#  Task list in with CPU % and memory in Windows PowerShell 
 ##################################################
 if ($args[0] -eq $null) {
     $SortCol = "Memory"
